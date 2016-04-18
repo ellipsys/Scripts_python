9286d11fb27cdbc559742a6291f2bfec948402c3
Eliseo Torres <eliseo.torres95@gmail.com>
2016-04-18 10:35:25 -0500

    Create write_matriz.py

---
+++ write_matriz.py
@@ -0,0 +1,9 @@
+matriz=[]
+filas=int(raw_input("Escriba la cantidad de filas: "))
+columnas=int(raw_input("Escriba la cantidad de columnas: "))
+for i in range(filas):
+	matriz.append([0]*columnas)
+for f in range(filas):
+	for c in range(columnas):
+		matriz[f][c]=int(raw_input("Escribe el elemento %d,%d: "%(f,c)))
+print matriz
