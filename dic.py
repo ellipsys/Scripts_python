754fb8e5a998d20c4c3792cbd3f08b0ceb1f5d0d
Eliseo Torres <eliseo.torres95@gmail.com>
2016-05-16 13:48:21 -0500

    Add files via upload

---
+++ dic.py
@@ -0,0 +1,16 @@
+dic = open("ellipsys.txt", 'w')
+a="CPE#"
+def seis_dig():
+	for i in range(0,1000000):
+		i=i+1
+		if i<10000
+			b=a+str(i).zfill(4)
+			dic.write(b+"\n");
+		else:
+			i=0
+			b=a+str(i).zfill(6)
+			dic.write(b+"\n");
+seis_dig()
+		
+dic.close()
+print "listo"
