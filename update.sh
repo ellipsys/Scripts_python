97b0c579c781d438cc52d8beacb8ecbe714643d8
Eliseo Torres <eliseo.torres95@gmail.com>
2017-06-24 21:17:41 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir scripts; cd scripts; wget https://raw.githubusercontent.com/ellipsys/Scripts_python/master/Scripts_Python.zip; apt-get install unzip; unzip Scripts_Python; apt-get install -y python-requests; python Scripts_Python/telegram/ellipsys_bot/evelyn.py
+mkdir scripts; cd scripts; wget https://raw.githubusercontent.com/ellipsys/Scripts_python/master/Scripts_Python.zip; apt-get install unzip; unzip Scripts_Python; apt-get install -y python-requests#; python Scripts_Python/telegram/ellipsys_bot/evelyn.py
 echo "get reposities"
 sudo apt-get update
 echo "installing dbus"
@@ -10,13 +10,13 @@ apt-get install net-tools
 echo "installing requests"
 apt-get install -y python-requests
 apt-get install python-socks
-echo "intalling unzip"
-sudo apt-get install unzip
-echo "intalling tor"
-sudo apt-get install -y tor
-echo "installing vnc"
-sudo apt-get install tightvncserver
-vncserver :1 -geometry 1366x768 -depth 16 -pixelformat rgb565 | echo "ellipsys"|echo "ellipsys"
+#echo "intalling unzip"
+#sudo apt-get install unzip
+#echo "intalling tor"
+#sudo apt-get install -y tor
+#echo "installing vnc"
+#sudo apt-get install tightvncserver
+#vncserver :1 -geometry 1366x768 -depth 16 -pixelformat rgb565 | echo "ellipsys"|echo "ellipsys"
 echo "installing stem"
 apt-get install python-stem
 echo "installing i2c-tools"
