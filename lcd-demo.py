3f6b0a40caa90f948860df3a2660d4022e598cbb
Eliseo Torres <eliseo.torres95@gmail.com>
2017-06-26 12:22:47 -0500

    Update lcd-demo.py

--- lcd-demo.py
+++ lcd-demo.py
@@ -4,20 +4,22 @@ import subprocess
 import time
 
 display = lcddriver.lcd()
-
-# Main body of code
 try:
+    display.lcd_clear()
     while True:
-        
-        ips = subprocess.check_output("hostname -I")#ifconfig eth0 | grep 'inet ' | awk -F'[: ]+' '{ print $3 }'", shell=True)
-        for ip in ips.splitlines():
-            
+        ip = subprocess.check_output("ifconfig eth0 | grep 'inet ' | awk -F'[: ]+' '{ print $3 }'", shell=True)
+        #for ip in ips.splitlines():#192.168.0.10
+        if len(str(ip)) > 11:
             display.lcd_display_string("Ip local", 1) 
             display.lcd_display_string(str(ip), 2) 
             time.sleep(2)
-            display.lcd_clear()                               
-                                                 
-
+            #display.lcd_clear()
+        else: 
+            ip = subprocess.check_output("ifconfig wlan0 | grep 'inet ' | awk -F'[: ]+' '{ print $3 }'", shell=True)
+            display.lcd_display_string("Ip wlan0", 1) 
+            display.lcd_display_string(str(ip), 2)                                      
+            time.sleep(2)
+            
 except: 
     display.lcd_display_string("Not connected",1)
-    display.lcd_clear()
+    
