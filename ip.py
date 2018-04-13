f4e6a57099c56078661af4da60db4aada3133001
Eliseo Torres <eliseo.torres95@gmail.com>
2018-04-13 14:13:41 -0500

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,21 +1,15 @@
-import requests
+import requests as rq
 import json
 
-a="181.54.153."
-c = open("eli.txt","w")
-def ip_search():
-	for i in range(0,255):
-		i=i+1
-		b=a+str(i)
-		r=requests.get("http://ipinfo.io/"+b+"/json")
-		h=r.json()
-		p=str(r.status_code)
-		if p=="200":
-                        c.write(b+"\n");
-                        c.write(str(h)+"\n"+"\n");
-                        print "En proceso "+b
-		
-		
-ip_search()
 
-print "listo"
+def main():
+	r=rq.get("http://ipinfo.io/"+b+"/json")
+	content=r.json()
+	code=str(r.status_code)
+	if p=="200":
+        	print "[+] You stay conected\n%s",%(content)
+		
+		
+if __name__=="__main__":
+	main()
+
